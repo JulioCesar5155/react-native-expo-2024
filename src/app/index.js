a3e3a714671b903c3769cd46fcb1129c64eb55a8
JulioCesar5155 <125457479+JulioCesar5155@users.noreply.github.com>
2024-09-03 12:52:07 +0000

    [feat] commit

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha primeira janela </Text>
+      <Text>Aplicativo Pronto Para Usar </Text>
       <StatusBar style="auto" />
     </View>
   );
